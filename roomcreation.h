e4ce0c273ea82641b801ccbd3284b0207caeb05b
Try6me <50838187+Try6me@users.noreply.github.com>
2019-05-27 17:11:22 +0200

    Create roomcreation.h

---
+++ roomcreation.h
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+
+
+//int main(void)
+void createroom()
+{       
+        char i = "V";
+        char tab[0]={"V"};
+        printf("Veuillez sélectionner la taille de la salle : ");
+        int nombre = 625 ;
+        int r = 0;
+        r = (int) sqrt(nombre);
+        int x=r;
+        int y=r;
+        if (r==r)
+                printf("%d\n",r);
+        else
+                return NULL;
+        tab[][]=
+}
+
+        /*for(i=0;i<x;i++){
+                for(i=0;i<y;i++){
+                        tab[i]="V";
+                }
+        }
+}       /*
+        if (nombre != NULL)
+        {
+                printf("ok");
+        }
+        else
+                printf("no");
+        return 0;*/
