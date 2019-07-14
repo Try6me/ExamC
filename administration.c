362b6598ef699dc00c4bf456d39027440e41dcfc
Try6me <50838187+Try6me@users.noreply.github.com>
2019-07-14 13:46:39 +0200

    Add files via upload

---
+++ administration.c
@@ -0,0 +1,47 @@
+#include "gestionCompte.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "gestionClient.h"
+#include <time.h>
+
+int countLineInFile(char * filename)
+{
+    int count = 0;
+    char line[1024];
+    FILE* fichier = fopen(filename, "r");
+    if(fichier != NULL)
+    {
+        while (fgets(line, 1024, fichier))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void interetsC()
+{
+    Compte * cmpt = malloc(countLineInFile("bddcomptes.csv"));
+    cmpt = chargementCmpt();
+    int nbCompte = countLineInFile("bddcomptes.csv");
+    int i = 0;
+    float totalSmall = 0;
+    float totalMedium = 0;
+    float totalBig = 0;
+}
+
+int SommeCompte()
+{
+    Compte * cmpt = malloc(countLineInFile("bddcomptes.csv"));
+    cmpt =chargementCmpt();
+
+    int nbCompte = countLineInFile("bddcomptes.csv");
+    int i,total = 0;
+
+        for(i=0;i<nbCompte;i++)
+        {
+            total += cmpt[i].solde;
+        }
+return total;
+}
---
+++ administration.h
@@ -0,0 +1,8 @@
+#ifndef ADMINISTRATION_H_INCLUDED
+#define ADMINISTRATION_H_INCLUDED
+int SommeCompte();
+void SommeCompteParType();
+void InteretCompteParType();
+
+
+#endif // ADMINISTRATION_H _INCLUDED
